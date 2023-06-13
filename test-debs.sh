9a95ddda6ffdd9e85232ed0731bc2d1e99c15c6d
Tianon Gravi <admwiggin@gmail.com>
2023-06-13 15:08:46 -0700

    Add basic package tests to GitHub Actions

---
+++ test-debs.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+dist="$1"; shift
+
+debs="$1"; shift
+[ -d "$debs" ]
+debs="$(readlink -ve "$debs")"
+
+img="debian:$dist"
+if ! docker image inspect --format '.' "$img" &> /dev/null; then
+	docker pull "$img"
+fi
+
+args=(
+	--interactive
+	--rm
+	--init
+	--name "test-moby-debs-$dist"
+	--mount "type=bind,src=$debs,dst=/debs,ro"
+	--privileged
+	--volume /var/lib/containerd
+	--volume /var/lib/docker
+	--tmpfs /run
+)
+
+if [ -t 0 ] && [ -t 1 ]; then
+	args+=( --tty )
+fi
+
+script="$(
+cat <<-'EOBASH'
+	# https://github.com/moby/moby/blob/38805f20f9bcc5e87869d6c79d432b166e1c88b4/hack/dind#L28-L38
+	if [ -f /sys/fs/cgroup/cgroup.controllers ]; then
+		mkdir -p /sys/fs/cgroup/init
+		xargs -rn1 < /sys/fs/cgroup/cgroup.procs > /sys/fs/cgroup/init/cgroup.procs || :
+		sed -e 's/ / +/g' -e 's/^/+/' < /sys/fs/cgroup/cgroup.controllers > /sys/fs/cgroup/cgroup.subtree_control
+	fi
+
+	apt-get update -qq
+	arch="$(dpkg --print-architecture)"
+	apt-get install -yqq --no-install-recommends -V ca-certificates iptables /debs/moby-*_"${arch}".deb
+
+	containerd --version
+	ctr --version
+	docker --version
+	docker buildx version
+	runc --version
+
+	containerd &
+	timeout 10s sh -c "while ! ctr version; do sleep 1; done"
+
+	ctr image pull docker.io/library/hello-world:latest
+	ctr run --rm docker.io/library/hello-world:latest hello
+
+	dockerd &
+	timeout 10s sh -c "while ! docker version; do sleep 1; done"
+
+	docker run --rm hello-world
+	docker run --rm --init hello-world
+
+	printf 'FROM hello-world' | DOCKER_BUILDKIT=0 docker build --pull -
+
+	printf 'FROM hello-world' | docker buildx build --pull -
+
+	docker images
+EOBASH
+)"
+
+exec docker run "${args[@]}" "$img" bash -Eeuo pipefail -xc "$script"
